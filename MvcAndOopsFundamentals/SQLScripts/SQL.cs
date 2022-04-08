e1c171fcc6df1f75fa5a1259684ed8c821b76a0d
harrygupta89 <harrysist@gmail.com>
2022-04-08 16:23:05 +0530

    SQL

---
+++ MvcAndOopsFundamentals/SQLScripts/SQL.cs
@@ -0,0 +1,17 @@
+﻿1.select * from customers where name like '%Joe'
+
+------------------------ -
+
+2.
+Select Product.Name from
+	OrderProduct join Order join Product
+	on OrderProduct.OrderId = Order.OrderId
+	and OrderProduct.ProductId = Order.ProductId
+	and Order.CustomerId = Customer.CustomerId
+
+where
+	Customer.Name = "Joe"
+	and CreatedAt > '11/1/2016'
+
+
+	
